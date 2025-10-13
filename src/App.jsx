92020aff84f95f1df371bcaabf37f45c2547543a
yams <sriyamini659@gmail.com>
2025-10-13 12:36:31 +0530

    commited all files

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+
+import './App.css';
+import './index.css';
+const App = () => {
+  return (
+    <div>
+      {/* Left Sidebar */}
+      <div className="sidebar">
+        <div>
+          <h1>OrderIn</h1>
+          <p>by</p>
+          <div/>
+          <p>PurnVasu Tech Solutions</p>
+        </div>
+        {/* Sushi & Phone Graphic Circle */}
+        <div>
+          {/* You can replace these with actual PNGs/icons or leave as placeholders */}
+          <div className='icon-circle'>
+            <img src="https://img.icons8.com/color/48/sushi.png" alt="Sushi" />
+          </div>
+         
+        </div>
+        <div className='footer-text'>
+          <span  />
+          <span>Personalized Restaurant Control Unit</span>
+        </div>
+      </div>
+
+      {/* Main Content */}
+      <div className='main-content'>
+        <h2>
+          <b>
+          WELCOME<br/>XYZ Restaurant</b>
+        </h2>
+        {/* Login Form */}
+        <div className='login-form'>
+          <h3>Login</h3>
+          <p>To your account to continue</p>
+          <form>
+            <input type="text" placeholder="User Id"  />
+            <input type="password" placeholder="Password" />
+            <button type="submit" >Password</button>
+          </form>
+        </div>
+        {/* Footer */}
+        <div className='footer'>
+          <div>Contact PurnVasu for queries:</div>
+          <div>Email: OrderIn.vap@gmail.com</div>
+        </div>
+        {/* Curved design */}
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
